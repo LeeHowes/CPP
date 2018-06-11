82a457e6780649002810adf509ef983cfe9b1aad
Lee Howes <lwh@fb.com>
2018-06-11 16:12:30 -0700

    Bulk driver interface with build_driver hook.

--- continuation_experiments/bulk_driver.cpp
+++ continuation_experiments/bulk_driver.cpp
@@ -3,24 +3,74 @@
 #include <optional>
 #include <iostream>
 
-// TODO: Move the drive functionality slowly into a driver class
-// Implement two versions of then_execute, one that passes its own and one
-// default
-struct DefaultDriver {
+// Custom default driver for this promise
+template<class PromiseT>
+struct DefaultDriverImpl {
+  void start() {
+    const auto& shape = promise_.get_shape();
+    for(int i = 0; i < shape; ++i) {
+      promise_.execute_at(i);
+    }
+    promise_.done();
+  }
 
+  void end() {
+  }
+
+  PromiseT& promise_;
 };
 
-template<class F, class OutputPromise, class Shape, class RF, class BulkDriver>
+struct DefaultDriver {
+  template<class PromiseT>
+  auto operator()(PromiseT& prom){
+    return DefaultDriverImpl<PromiseT>{prom};
+  }
+  template<class PromiseT, class ShapeF, class AtF, class DoneF>
+  auto operator()(PromiseT& prom, ShapeF&&, AtF&&, DoneF&&){
+    return DefaultDriverImpl<PromiseT>{prom};
+  }
+};
+
+
+template<class PromiseT, class ShapeF, class AtF, class DoneF>
+struct EndDriverImpl {
+  void start() {
+  }
+
+  void end() {
+    const auto& shape = shapeF_();
+    for(std::decay_t<decltype(shape)> i = 0; i < shape; ++i) {
+      atF_(i);
+    }
+    doneF_();
+  }
+
+  PromiseT& promise_;
+  ShapeF shapeF_;
+  AtF atF_;
+  DoneF doneF_;
+};
+
+struct EndDriver {
+  template<class PromiseT, class ShapeF, class AtF, class DoneF>
+  auto operator()(PromiseT& prom, ShapeF&& shapeF, AtF&& atF, DoneF&& doneF){
+    return EndDriverImpl<PromiseT, ShapeF, AtF, DoneF>{
+      prom,
+      std::forward<ShapeF>(shapeF),
+      std::forward<AtF>(atF),
+      std::forward<DoneF>(doneF)};
+  }
+};
+
+template<class F, class OutputPromise, class Shape, class RF>
 class InputPromise {
 public:
   InputPromise(
-      F&& f, OutputPromise&& outputPromise, int initialResult, int shape,
-      BulkDriver&& bulkDriver) :
+      F&& f, OutputPromise&& outputPromise, int initialResult, int shape) :
       f_(std::move(f)),
       outputPromise_(std::move(outputPromise)),
       result_{initialResult},
-      shape_{shape},
-      bulkDriver_{std::forward<BulkDriver>(bulkDriver)} {}
+      shape_{shape} {}
 
   void set_value(int value) {
     inputValue_ = std::move(value);
@@ -29,6 +79,26 @@ public:
   void set_exception(std::exception_ptr e) {
     outputException_ = std::move(e);
   }
+
+  template<class BulkDriver = DefaultDriver>
+  auto build_driver(BulkDriver driver = DefaultDriver{}) {
+    return driver(*
+      this,
+      [this](){return this->get_shape();},
+      [this](auto i){this->execute_at(i);},
+      [this](){this->done();});
+  }
+
+private:
+  F f_;
+  OutputPromise outputPromise_;
+  std::optional<int> inputValue_;
+  std::optional<std::exception_ptr> outputException_;
+  int result_;
+  int shape_;
+
+
+  friend struct DefaultDriverImpl<InputPromise>;
 
   const int& get_shape() const {
     return shape_;
@@ -47,15 +117,6 @@ public:
       outputPromise_.set_value(std::move(result_));
     }
   }
-
-private:
-  F f_;
-  OutputPromise outputPromise_;
-  std::optional<int> inputValue_;
-  std::optional<std::exception_ptr> outputException_;
-  int result_;
-  int shape_;
-  BulkDriver bulkDriver_;
 };
 
 
@@ -67,23 +128,20 @@ auto bulk_then_value(
   return [continuationFunction = std::forward<F>(continuationFunction),
           resultFactory = std::forward<RF>(resultFactory),
           s = std::forward<Shape>(s)](
-        auto&& outputPromise,
-        auto&& bulkDriver) mutable {
+        auto&& outputPromise) mutable {
     using OutputPromiseRef = decltype(outputPromise);
     using OutputPromise = typename std::remove_reference<OutputPromiseRef>::type;
-    using BulkDriverRef = decltype(bulkDriver);
-    using BulkDriver = typename std::remove_reference<BulkDriverRef>::type;;
 
-    return InputPromise<F, OutputPromise, Shape, RF, BulkDriver>(
+    return InputPromise<F, OutputPromise, Shape, RF>(
         std::move(continuationFunction),
         std::forward<OutputPromiseRef>(outputPromise),
         resultFactory(),
-        s,
-        std::forward<BulkDriverRef>(bulkDriver));
+        s);
   };
 
 }
 
+struct SimpleExecutor {
 template<class Continuation>
 int then_execute(Continuation&& cont, int inputFuture) {
   class Promise {
@@ -107,25 +165,71 @@ int then_execute(Continuation&& cont, int inputFuture) {
   int resultStorage;
   std::optional<std::exception_ptr> exceptionStorage;
   auto boundCont = std::forward<Continuation>(cont)(
-    Promise{resultStorage, exceptionStorage}, DefaultDriver{});
+    Promise{resultStorage, exceptionStorage});
 
-  const auto& shape = boundCont.get_shape();
+  // Use default driver for this executor
+  auto driver = boundCont.build_driver();
   boundCont.set_value(inputFuture);
-  for(int i = 0; i < shape; ++i) {
-    boundCont.execute_at(i);
-  }
-  boundCont.done();
+  driver.start();
+  driver.end();
   return resultStorage;
 }
+};
+
+struct BulkExecutor {
+template<class Continuation>
+int then_execute(Continuation&& cont, int inputFuture) {
+  class Promise {
+  public:
+      Promise(int& resultStorage, std::optional<std::exception_ptr>& exceptionStorage) : resultStorage_(resultStorage), exceptionStorage_(exceptionStorage) {}
+
+      void set_value(int value) {
+          resultStorage_ = value;
+      }
+
+      void set_exception(std::exception_ptr e) {
+          exceptionStorage_ = std::move(e);
+      }
+
+  private:
+      int& resultStorage_;
+      std::optional<std::exception_ptr>& exceptionStorage_;
+  };
+
+
+  int resultStorage;
+  std::optional<std::exception_ptr> exceptionStorage;
+  auto boundCont = std::forward<Continuation>(cont)(
+    Promise{resultStorage, exceptionStorage});
+
+  // This executor uses the end driver
+  auto driver = boundCont.build_driver(EndDriver{});
+  boundCont.set_value(inputFuture);
+  driver.start();
+  driver.end();
+  return resultStorage;
+}
+};
 
 int main() {
-  auto inputFuture = 2;
-  auto p = bulk_then_value(
-      [](const int& a, int /*idx*/, int &out){out+=a;},
-      int{20}, // Shape
-      []() -> int {return 0;});
-  auto resultF = then_execute(p, inputFuture);
-  std::cout << resultF << "\n";
+  {
+    auto inputFuture = 2;
+    auto p = bulk_then_value(
+        [](const int& a, int /*idx*/, int &out){out+=a;},
+        int{20}, // Shape
+        []() -> int {return 0;});
+    auto resultF = SimpleExecutor{}.then_execute(p, inputFuture);
+    std::cout << resultF << "\n";
+  }
+  {
+    auto inputFuture = 2;
+    auto p = bulk_then_value(
+        [](const int& a, int /*idx*/, int &out){out+=a;},
+        int{20}, // Shape
+        []() -> int {return 0;});
+    auto resultF = BulkExecutor{}.then_execute(p, inputFuture);
+    std::cout << resultF << "\n";
+  }
 
-  return resultF;
+  return 0;
 }
