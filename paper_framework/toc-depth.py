1cf02bba3bd5c2343c14e73da360a5dc8c6e1095
Lee Howes <lwh@fb.com>
2022-05-13 17:07:27 -0700

    Remove old paper framework

--- paper_framework/toc-depth.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-import yaml
-
-frontmatter = next(yaml.safe_load_all(sys.stdin))
-toc_depth = frontmatter.get('toc-depth')
-if toc_depth is not None:
-    print(toc_depth)
